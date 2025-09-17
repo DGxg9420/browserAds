64aad71095314c0aa73dc0e4cad42197ed93cad1
ssson <ssson966@gmail.com>
2025-09-18 02:16:29 +0800

    浏览器多开运行

---
+++ demo2.py
@@ -0,0 +1,55 @@
+from  core.utils import get_proxy_info
+from core.browserOperation import BrowserOperationOnWebtraficRu
+from core.utils import get_proxy_raw_by_api
+from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
+
+
+def run_browser_operation(_result_queue: Queue, _run_state: dict[str, bool], _blog_url: str):
+    while True:
+        if not _run_state["state"]:
+            break
+        proxy_raw = get_proxy_raw_by_api()
+        if not proxy_raw:
+            print("没有可用的代理")
+            continue
+        proxy_info = get_proxy_info(proxy_raw)
+        if not proxy_info:
+            print("代理信息获取失败")
+            continue
+
+        browser_operation = BrowserOperationOnWebtraficRu(proxy_info=proxy_info, play_url=_blog_url)
+        try:
+            result = browser_operation.startBrowserAds()
+            print(result)
+            _result_queue.put(result)
+            browser_operation.browserClose()
+        except Exception as e:
+            print(e)
+            _result_queue.put(False)
+            browser_operation.browserClose()
+
+
+def count_success_count(_result_queue: Queue, length: int=100):
+    success_count = 0
+    while True:
+        if success_count >= length:
+            print("\n任务完成！")
+            run_state["state"] = False
+            break
+        result = _result_queue.get()
+        if result:
+            success_count += 1
+            print(f"\r成功次数：{success_count}", end="")
+
+
+if __name__ == '__main__':
+    run_state: dict[str, bool] = {"state": True}
+    result_queue: Queue[bool] = Queue()
+    blog_url = "https://ssson966.blogspot.com/2025/09/linuxerror-while-loading-shared.html"
+    with ThreadPoolExecutor(max_workers=21) as executor:
+        for i in range(20):
+            executor.submit(run_browser_operation, result_queue, run_state, blog_url)
+        #  启动计数线程
+        executor.submit(count_success_count, result_queue)
+
