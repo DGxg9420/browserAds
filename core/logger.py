3da00e761407b25e5e0e2e60579ad1e011000437
ssson <ssson966@gmail.com>
2025-09-18 23:20:15 +0800

    添加日志

---
+++ core/logger.py
@@ -0,0 +1,34 @@
+from core.utils import get_full_path
+import os
+import sys
+import logging
+from logging.handlers import RotatingFileHandler
+
+def setup_logger(app_name: str = "app"):
+
+    _logger = logging.getLogger(app_name)
+    _logger.setLevel(logging.DEBUG)
+    _logger.propagate = False
+
+    # 创建控制台处理器
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    console_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    console_handler.setFormatter(console_format)
+    _logger.addHandler(console_handler)
+
+    # Create a file handler
+    dir_log = get_full_path("logs")
+    if not os.path.exists(dir_log):
+        os.makedirs(dir_log)
+
+    file_handler = RotatingFileHandler(dir_log, maxBytes=1024 * 1024 * 20, backupCount=3, encoding='utf-8', mode="a")
+    file_handler.setLevel(logging.ERROR)
+    file_format = logging.Formatter('%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler.setFormatter(file_format)
+    _logger.addHandler(file_handler)
+
+    return _logger
+
+
+logger = setup_logger(app_name="Ads-Browser-View")
